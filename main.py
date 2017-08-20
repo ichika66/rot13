47a8f2f6a5780af0b3e7a0905cf49a4495205728
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-08-20 20:35:23 +0200

    Add base code to main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+
+import jinja2
+import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class Rot13Handler(Handler):
+	def get(self):
+		self.render('rot13.heml')
+
+	def post(self):
+		rot13 = ''
+		text = self.request.get('text')
+		if text:
+			rot13 = text.encode('rot13')
+
+		self.render('rot13.html', text = rot13)
+
+app = webapp2.WSGIApplication([('/rot13', Rot13Handler),
+								],
+								debug=True)
